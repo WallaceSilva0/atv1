c8b616a2f70224707b30018a5f0f73555dd2cd2e
Wallace Nunes da Silva <wallacenunes3372@gmail.com>
2025-03-31 20:41:30 -0300

    Add files via upload

---
+++ atividade-01.py
@@ -0,0 +1,10 @@
+# Atividade 01- WALLACE NUNES DA SILVA
+
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+# Calculo do IMC
+imc = peso / (altura * altura)
+
+# Resultado final
+print("Seu IMC é:", imc)
